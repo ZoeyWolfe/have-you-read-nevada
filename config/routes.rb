73d7ce57f1b4d126f4d67d9fd3df2b21e77e716b
Teagan Widmer <teagan@futureadvisor.com>
2015-11-13 13:58:47 -0800

    slight refactor to Github Pages

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'pdf'  => 'nevada#pdf'
-  get 'epub'  => 'nevada#epub'
-  get 'mobi'  => 'nevada#mobi'
-
-  root 'welcome#index'
-
-  get '*path' => redirect('/')
-end
